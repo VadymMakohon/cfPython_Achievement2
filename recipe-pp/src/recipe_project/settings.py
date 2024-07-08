b3cb68df981ca4ed6be7fa228b66b98466754136
VadymMakohon <errorks7@gmail.com>
2024-07-08 17:43:17 +0200

    added images

--- recipe-pp/src/recipe_project/settings.py
+++ recipe-pp/src/recipe_project/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'  # Adjust as needed
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Directory where your static files are located
+]
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'

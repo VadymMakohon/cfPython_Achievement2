60b9e6826a6face927b5c13a1614002cff270213
VadymMakohon <errorks7@gmail.com>
2024-07-08 21:02:54 +0200

    updated pictures path

--- recipe-pp/src/recipe_project/settings.py
+++ recipe-pp/src/recipe_project/settings.py
@@ -118,14 +118,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+import os
+
 STATIC_URL = '/static/'  # Adjust as needed
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Directory where your static files are located
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- recipe-pp/src/recipes/models.py
+++ recipe-pp/src/recipes/models.py
@@ -7,7 +7,7 @@ class Recipe(models.Model):
     description = models.TextField(default="this recipe doesn't have a description yet")
     difficulty = models.CharField(max_length=20, default="N/A")
     ingredients = models.ManyToManyField('ingredients.Ingredient', through='recipesingredients.RecipeIngredient', related_name='recipes')
-    pic = models.ImageField(upload_to='recipes', default='no_picture.png') # default stored in src/media
+    pic = models.ImageField(upload_to='recipes/') # default stored in src/media
 
     def __str__(self):
         return self.name

bd4454e009c4df58a46b115d66109f88b562eab9
VadymMakohon <errorks7@gmail.com>
2024-07-10 23:44:18 +0200

    added bar, charts and line

--- recipe-app/src/recipes/urls.py
+++ recipe-app/src/recipes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import recipes_home, export_recipes_csv, generate_chart, add_recipe
+from .views import recipes_home, export_recipes_csv, about_page, generate_chart, add_recipe
 from .views import RecipesListView, RecipesDetailView
 from django.conf.urls.static import static
 from django.conf import settings
@@ -13,6 +13,7 @@ urlpatterns = [
     path("generate-chart/", generate_chart, name="generate_chart"),
     path("recipes/", RecipesListView.as_view(), name="recipes_list"),
     path("list/<pk>", RecipesDetailView.as_view(), name="detail"),
+    path("about/", about_page, name="about"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

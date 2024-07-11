34ad470bfdf2c94d8c05fd5f4193b85c2e42d896
VadymMakohon <errorks7@gmail.com>
2024-07-11 14:27:34 +0200

    updated settings.py

--- recipe-app/src/recipe_project/settings.py
+++ recipe-app/src/recipe_project/settings.py
@@ -26,12 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "desolate-meadow-45396-42aa33eddc2d.herokuapp.com",
-    ".vercel.app",
-    "127.0.0.1",
-    "localhost",
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

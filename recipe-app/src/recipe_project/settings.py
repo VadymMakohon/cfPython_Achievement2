0cdf6637517fc8eb5f5b0a702a6e642522072470
VadymMakohon <errorks7@gmail.com>
2024-07-12 14:57:20 +0200

    updated setting and procfile

--- recipe-app/src/recipe_project/settings.py
+++ recipe-app/src/recipe_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpevh4xz)3w)6xuq6kv7g&3^xf^)gr-n3&p#%9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 

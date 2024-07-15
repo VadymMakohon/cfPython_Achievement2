f62b8427e202312f388d4f400f5702c50a85aea2
Vadym Makohon <138728243+VadymMakohon@users.noreply.github.com>
2024-07-15 07:59:59 +0200

    Update wsgi.py

--- recipe-app/recipe_project/wsgi.py
+++ recipe-app/recipe_project/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for recipe_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_project.settings')
-
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'media'))
+application.add_files(os.path.join(BASE_DIR, 'media'), prefix='media/')

8b15bb9e66b573ae60175c40e6811d4e9edd8e87
VadymMakohon <errorks7@gmail.com>
2024-09-14 12:41:20 +0200

    minor updates

--- recipe-app/recipe_project/settings.py
+++ recipe-app/recipe_project/settings.py
@@ -121,8 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'  # Adjust as needed
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# Directory where static files are collected for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Directories where Django will look for static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # e.g., for your project's static files
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

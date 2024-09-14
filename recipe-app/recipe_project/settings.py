6c325c6e4311399dfd09a7579c73c305912f9ec3
VadymMakohon <errorks7@gmail.com>
2024-09-14 12:40:06 +0200

    minor updates

--- recipe-app/recipe_project/settings.py
+++ recipe-app/recipe_project/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'  # Adjust as needed
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
